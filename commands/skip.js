b7031b431685d9ed86123f24e57f6813315bfae2
fbdndevelopment <ghoulremixes@gmail.com>
2026-03-19 20:59:49 +0000

    Delete commands directory

--- commands/skip.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  name: "skip",
-  execute(message) {
-    message.reply("⏭️ Skip not implemented yet.");
-  }
-};
--- commands/stop.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  name: "stop",
-  execute(message) {
-    message.reply("⏹️ Stop not implemented yet.");
-  }
-};
